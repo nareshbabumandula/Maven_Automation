19add1b9983f3b57903ea726ffe961a61203d86d
Mandula Naresh Babu <naresh2223@gmail.com>
2016-12-21 21:05:23 -0500

    Added comments for TC01

--- src/test/java/scripts/TC01.java
+++ src/test/java/scripts/TC01.java
@@ -9,13 +9,21 @@ public class TC01 extends ActionDriver{
   @Test
   public void testTC01() throws InterruptedException {
 	 logger = report.startTest("TC01");
-	 launchApplication("http://www.rightstart.com/"); 
-	 type(HomePage.txtSearch, "toys","Search Textbox");
-	 click(HomePage.btnSearch,"Search Button");
+
+//Access Right Start Site	 
+launchApplication("http://www.rightstart.com/"); 
+
+//Enter the value in Search textbox	 
+type(HomePage.txtSearch, "toys","Search Textbox");
+	 
+click(HomePage.btnSearch,"Search Button");
 	
-	 Thread.sleep(5000);
-	 report.endTest(logger);
-	 report.flush();
+	 
+Thread.sleep(5000);
+	 
+report.endTest(logger);
+	 
+report.flush();
 	  
   }
 }
